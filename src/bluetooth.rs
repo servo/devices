c0af8aea11ceefd79069f56448b5edcc8e7cab26
mrmiywj <jsyangwenjie@gmail.com>
2016-05-04 11:34:40 +0800

    add error function for empty structs on non-linux platforms

--- src/bluetooth.rs
+++ src/bluetooth.rs
@@ -23,16 +23,336 @@ const NOT_SUPPORTED_ERROR: &'static str = "Error! Not supported platform!";
 struct BluetoothAdapterBluez {}
 
 #[cfg(not(target_os = "linux"))]
+impl BluetoothAdapterBluez {
+    pub fn get_object_path(&self) -> Result<String, Box<Error>>{
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_address(&self) -> Result<String, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_name(&self) -> Result<String, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_alias(&self) -> Result<String, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn set_alias(&self, value: String) -> Result<(), Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_class(&self) -> Result<u32, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn is_powered(&self) -> Result<bool, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn set_powered(&self, value: bool) -> Result<(), Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn is_discoverable(&self) -> Result<bool, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn set_discoverable(&self, value: bool) -> Result<(), Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn is_pairable(&self) -> Result<bool, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn set_pairable(&self, value: bool) -> Result<(), Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_pairable_timeout(&self) -> Result<u32, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_discoverable_timeout(&self) -> Result<u32, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn set_discoverable_timeout(&self, value: u32) -> Result<(), Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn is_discovering(&self) -> Result<bool, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_uuids(&self) -> Result<Vec<String>, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_vendor_id_source(&self) -> Result<String, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_vendor_id(&self) -> Result<u32, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_product_id(&self) -> Result<u32, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_device_id(&self) -> Result<u32, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_modalias(&self) -> Result<(String, u32, u32, u32), Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn start_discovery(&self) -> Result<(), Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn stop_discovery(&self) -> Result<(), Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+}
+
+#[cfg(not(target_os = "linux"))]
 struct BluetoothDeviceBluez {}
+
+#[cfg(not(target_os = "linux"))]
+impl BluetoothDeviceBluez {
+    pub fn get_object_path(&self) -> Result<String, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_address(&self) -> Result<String, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_name(&self) -> Result<String, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_icon(&self) -> Result<String, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_class(&self) -> Result<u32, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_appearance(&self) -> Result<u16, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_uuids(&self) -> Result<Vec<String>, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn is_paired(&self) -> Result<bool, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn is_connected(&self) -> Result<bool, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn is_trusted(&self) -> Result<bool, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn is_blocked(&self) -> Result<bool, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_alias(&self) -> Result<String, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn set_alias(&self, value: String) -> Result<(), Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_adapter(&self) -> Result<String, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn is_legacy_pairing(&self) -> Result<bool, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_vendor_id_source(&self) -> Result<String, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_vendor_id(&self) -> Result<u32, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_product_id(&self) -> Result<u32, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_device_id(&self) -> Result<u32, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_modalias(&self) -> Result<(String, u32, u32, u32), Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_rssi(&self) -> Result<i16, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_tx_power(&self) -> Result<i16, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_gatt_services(&self) -> Result<Vec<BluetoothGATTService>, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn connect(&self) -> Result<(), Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn disconnect(&self) -> Result<(), Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn connect_profile(&self, uuid: String) -> Result<(), Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn disconnect_profile(&self, uuid: String) -> Result<(), Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn pair(&self) -> Result<(), Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn cancel_pairing(&self) -> Result<(), Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+}
 
 #[cfg(not(target_os = "linux"))]
 struct BluetoothGATTCharacteristicBluez {}
 
 #[cfg(not(target_os = "linux"))]
+impl BluetoothGATTCharacteristicBluez {
+    pub fn get_object_path(&self) -> Result<String, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_uuid(&self) -> Result<String, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn is_primary(&self) -> Result<bool, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_device(&self) -> Result<String, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_gatt_characteristics(&self) -> Result<Vec<BluetoothGATTCharacteristic>, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+}
+
+#[cfg(not(target_os = "linux"))]
 struct BluetoothGATTDescriptorBluez {}
 
 #[cfg(not(target_os = "linux"))]
+impl BluetoothGATTDescriptorBluez {
+    pub fn get_object_path(&self) -> Result<String, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_uuid(&self) -> Result<String, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_service(&self) -> Result<String, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_value(&self) -> Result<Vec<u8>, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn is_notifying(&self) -> Result<bool, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_flags(&self) -> Result<Vec<String>, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_gatt_descriptors(&self) -> Result<Vec<BluetoothGATTDescriptor>, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn read_value(&self) -> Result<Vec<u8>, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn write_value(&self, _values: Vec<u8>) -> Result<(), Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn start_notify(&self) -> Result<(), Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn stop_notify(&self) -> Result<(), Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+}
+
+#[cfg(not(target_os = "linux"))]
 struct BluetoothGATTServiceBluez {}
+
+#[cfg(not(target_os = "linux"))]
+impl BluetoothGATTServiceBluez {
+    pub fn get_object_path(&self) -> Result<String, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_uuid(&self) -> Result<String, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_characteristic(&self) -> Result<String, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_value(&self) -> Result<Vec<u8>, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn get_flags(&self) -> Result<Vec<String>, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn read_value(&self) -> Result<Vec<u8>, Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+
+    pub fn write_value(&self, _values: Vec<u8>) -> Result<(), Box<Error>> {
+        Err(Box::from(NOT_SUPPORTED_ERROR))
+    }
+}
 
 #[derive(Clone, Debug)]
 pub struct BluetoothAdapter {
