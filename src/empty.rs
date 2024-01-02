9df13dbd3b36b65e5754fe03545b1a34fda1e86c
Martin Robinson <mrobinson@igalia.com>
2024-01-02 11:05:33 +0100

    Upgrade to Rust 2018 edition
    In addition to fixing all of the compilation errors for the upgrade to
    Rust 2018, this change also reverts the conversion of BluetoothAdapter
    into a trait. This was never integrated into Servo.
    
    In newer versions of Rust, it seems harder to put a `dyn` trait
    implementation into an Option since it isn't sized. In addition, the
    trait-based implementation leads to a lot of duplicated code. There is
    probably a better way to hide the implementation details of the
    bluetooth crate than using traits here.
    
    In fact, a good long-term approach is probably to remove this crate
    entirely and switch to using a platform-independent bluetooth
    implementation which exists for Rust now. In the meantime, this will
    allow Servo to use the newest version of this crate, which is doesn't
    yet.

--- src/empty.rs
+++ src/empty.rs
@@ -12,7 +12,7 @@ const NOT_SUPPORTED_ERROR: &'static str = "Error! Not supported platform!";
 pub struct EmptyAdapter { }
 
 impl EmptyAdapter {
-    pub fn init() -> Result<EmptyAdapter, Box<Error>> {
+    pub fn init() -> Result<EmptyAdapter, Box<dyn Error>> {
         Ok(EmptyAdapter::new())
     }
 
@@ -24,95 +24,95 @@ impl EmptyAdapter {
         String::new()
     }
 
-    pub fn get_device_list(&self) -> Result<Vec<String>, Box<Error>> {
+    pub fn get_device_list(&self) -> Result<Vec<String>, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn get_address(&self) -> Result<String, Box<Error>> {
+    pub fn get_address(&self) -> Result<String, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn get_name(&self) -> Result<String, Box<Error>> {
+    pub fn get_name(&self) -> Result<String, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn get_alias(&self) -> Result<String, Box<Error>> {
+    pub fn get_alias(&self) -> Result<String, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn set_alias(&self, _value: String) -> Result<(), Box<Error>> {
+    pub fn set_alias(&self, _value: String) -> Result<(), Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn get_class(&self) -> Result<u32, Box<Error>> {
+    pub fn get_class(&self) -> Result<u32, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn is_powered(&self) -> Result<bool, Box<Error>> {
+    pub fn is_powered(&self) -> Result<bool, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn set_powered(&self, _value: bool) -> Result<(), Box<Error>> {
+    pub fn set_powered(&self, _value: bool) -> Result<(), Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn is_discoverable(&self) -> Result<bool, Box<Error>> {
+    pub fn is_discoverable(&self) -> Result<bool, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn set_discoverable(&self, _value: bool) -> Result<(), Box<Error>> {
+    pub fn set_discoverable(&self, _value: bool) -> Result<(), Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn is_pairable(&self) -> Result<bool, Box<Error>> {
+    pub fn is_pairable(&self) -> Result<bool, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn set_pairable(&self, _value: bool) -> Result<(), Box<Error>> {
+    pub fn set_pairable(&self, _value: bool) -> Result<(), Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn get_pairable_timeout(&self) -> Result<u32, Box<Error>> {
+    pub fn get_pairable_timeout(&self) -> Result<u32, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn set_pairable_timeout(&self, _value: u32) -> Result<(), Box<Error>> {
+    pub fn set_pairable_timeout(&self, _value: u32) -> Result<(), Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn get_discoverable_timeout(&self) -> Result<u32, Box<Error>> {
+    pub fn get_discoverable_timeout(&self) -> Result<u32, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn set_discoverable_timeout(&self, _value: u32) -> Result<(), Box<Error>> {
+    pub fn set_discoverable_timeout(&self, _value: u32) -> Result<(), Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn is_discovering(&self) -> Result<bool, Box<Error>> {
+    pub fn is_discovering(&self) -> Result<bool, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn get_uuids(&self) -> Result<Vec<String>, Box<Error>> {
+    pub fn get_uuids(&self) -> Result<Vec<String>, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn get_vendor_id_source(&self) -> Result<String, Box<Error>> {
+    pub fn get_vendor_id_source(&self) -> Result<String, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn get_vendor_id(&self) -> Result<u32, Box<Error>> {
+    pub fn get_vendor_id(&self) -> Result<u32, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn get_product_id(&self) -> Result<u32, Box<Error>> {
+    pub fn get_product_id(&self) -> Result<u32, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn get_device_id(&self) -> Result<u32, Box<Error>> {
+    pub fn get_device_id(&self) -> Result<u32, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn get_modalias(&self) -> Result<(String, u32, u32, u32), Box<Error>> {
+    pub fn get_modalias(&self) -> Result<(String, u32, u32, u32), Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 }
@@ -121,15 +121,15 @@ impl EmptyAdapter {
 pub struct BluetoothDiscoverySession { }
 
 impl BluetoothDiscoverySession {
-    pub fn create_session(_adapter: Arc<EmptyAdapter>) -> Result<BluetoothDiscoverySession, Box<Error>> {
+    pub fn create_session(_adapter: Arc<EmptyAdapter>) -> Result<BluetoothDiscoverySession, Box<dyn Error>> {
         Ok(BluetoothDiscoverySession{ })
     }
 
-    pub fn start_discovery(&self) -> Result<(), Box<Error>> {
+    pub fn start_discovery(&self) -> Result<(), Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn stop_discovery(&self) -> Result<(), Box<Error>> {
+    pub fn stop_discovery(&self) -> Result<(), Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 }
@@ -146,119 +146,119 @@ impl BluetoothDevice {
         String::new()
     }
 
-    pub fn get_address(&self) -> Result<String, Box<Error>> {
+    pub fn get_address(&self) -> Result<String, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn get_name(&self) -> Result<String, Box<Error>> {
+    pub fn get_name(&self) -> Result<String, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn get_icon(&self) -> Result<String, Box<Error>> {
+    pub fn get_icon(&self) -> Result<String, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn get_class(&self) -> Result<u32, Box<Error>> {
+    pub fn get_class(&self) -> Result<u32, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn get_appearance(&self) -> Result<u16, Box<Error>> {
+    pub fn get_appearance(&self) -> Result<u16, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn get_uuids(&self) -> Result<Vec<String>, Box<Error>> {
+    pub fn get_uuids(&self) -> Result<Vec<String>, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn is_paired(&self) -> Result<bool, Box<Error>> {
+    pub fn is_paired(&self) -> Result<bool, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn is_connected(&self) -> Result<bool, Box<Error>> {
+    pub fn is_connected(&self) -> Result<bool, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn is_trusted(&self) -> Result<bool, Box<Error>> {
+    pub fn is_trusted(&self) -> Result<bool, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn is_blocked(&self) -> Result<bool, Box<Error>> {
+    pub fn is_blocked(&self) -> Result<bool, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn get_alias(&self) -> Result<String, Box<Error>> {
+    pub fn get_alias(&self) -> Result<String, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn set_alias(&self, _value: String) -> Result<(), Box<Error>> {
+    pub fn set_alias(&self, _value: String) -> Result<(), Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn is_legacy_pairing(&self) -> Result<bool, Box<Error>> {
+    pub fn is_legacy_pairing(&self) -> Result<bool, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn get_vendor_id_source(&self) -> Result<String, Box<Error>> {
+    pub fn get_vendor_id_source(&self) -> Result<String, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn get_vendor_id(&self) -> Result<u32, Box<Error>> {
+    pub fn get_vendor_id(&self) -> Result<u32, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn get_product_id(&self) -> Result<u32, Box<Error>> {
+    pub fn get_product_id(&self) -> Result<u32, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn get_device_id(&self) -> Result<u32, Box<Error>> {
+    pub fn get_device_id(&self) -> Result<u32, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn get_modalias(&self) -> Result<(String, u32, u32, u32), Box<Error>> {
+    pub fn get_modalias(&self) -> Result<(String, u32, u32, u32), Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn get_rssi(&self) -> Result<i16, Box<Error>> {
+    pub fn get_rssi(&self) -> Result<i16, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn get_tx_power(&self) -> Result<i16, Box<Error>> {
+    pub fn get_tx_power(&self) -> Result<i16, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn get_manufacturer_data(&self) -> Result<HashMap<u16, Vec<u8>>, Box<Error>> {
+    pub fn get_manufacturer_data(&self) -> Result<HashMap<u16, Vec<u8>>, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn get_service_data(&self) -> Result<HashMap<String, Vec<u8>>, Box<Error>> {
+    pub fn get_service_data(&self) -> Result<HashMap<String, Vec<u8>>, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn get_gatt_services(&self) -> Result<Vec<String>, Box<Error>> {
+    pub fn get_gatt_services(&self) -> Result<Vec<String>, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn connect(&self) -> Result<(), Box<Error>> {
+    pub fn connect(&self) -> Result<(), Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn disconnect(&self) -> Result<(), Box<Error>> {
+    pub fn disconnect(&self) -> Result<(), Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn connect_profile(&self, _uuid: String) -> Result<(), Box<Error>> {
+    pub fn connect_profile(&self, _uuid: String) -> Result<(), Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn disconnect_profile(&self, _uuid: String) -> Result<(), Box<Error>> {
+    pub fn disconnect_profile(&self, _uuid: String) -> Result<(), Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn pair(&self) -> Result<(), Box<Error>> {
+    pub fn pair(&self) -> Result<(), Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn cancel_pairing(&self) -> Result<(), Box<Error>> {
+    pub fn cancel_pairing(&self) -> Result<(), Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 }
@@ -275,19 +275,19 @@ impl BluetoothGATTService {
         String::new()
     }
 
-    pub fn get_uuid(&self) -> Result<String, Box<Error>> {
+    pub fn get_uuid(&self) -> Result<String, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn is_primary(&self) -> Result<bool, Box<Error>> {
+    pub fn is_primary(&self) -> Result<bool, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn get_includes(&self) -> Result<Vec<String>, Box<Error>> {
+    pub fn get_includes(&self) -> Result<Vec<String>, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn get_gatt_characteristics(&self) -> Result<Vec<String>, Box<Error>> {
+    pub fn get_gatt_characteristics(&self) -> Result<Vec<String>, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 }
@@ -304,39 +304,39 @@ impl BluetoothGATTCharacteristic {
         String::new()
     }
 
-    pub fn get_uuid(&self) -> Result<String, Box<Error>> {
+    pub fn get_uuid(&self) -> Result<String, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn get_value(&self) -> Result<Vec<u8>, Box<Error>> {
+    pub fn get_value(&self) -> Result<Vec<u8>, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn is_notifying(&self) -> Result<bool, Box<Error>> {
+    pub fn is_notifying(&self) -> Result<bool, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn get_flags(&self) -> Result<Vec<String>, Box<Error>> {
+    pub fn get_flags(&self) -> Result<Vec<String>, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn get_gatt_descriptors(&self) -> Result<Vec<String>, Box<Error>> {
+    pub fn get_gatt_descriptors(&self) -> Result<Vec<String>, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn read_value(&self) -> Result<Vec<u8>, Box<Error>> {
+    pub fn read_value(&self) -> Result<Vec<u8>, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn write_value(&self, _values: Vec<u8>) -> Result<(), Box<Error>> {
+    pub fn write_value(&self, _values: Vec<u8>) -> Result<(), Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn start_notify(&self) -> Result<(), Box<Error>> {
+    pub fn start_notify(&self) -> Result<(), Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn stop_notify(&self) -> Result<(), Box<Error>> {
+    pub fn stop_notify(&self) -> Result<(), Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 }
@@ -353,23 +353,23 @@ impl BluetoothGATTDescriptor {
         String::new()
     }
 
-    pub fn get_uuid(&self) -> Result<String, Box<Error>> {
+    pub fn get_uuid(&self) -> Result<String, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn get_value(&self) -> Result<Vec<u8>, Box<Error>> {
+    pub fn get_value(&self) -> Result<Vec<u8>, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn get_flags(&self) -> Result<Vec<String>, Box<Error>> {
+    pub fn get_flags(&self) -> Result<Vec<String>, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn read_value(&self) -> Result<Vec<u8>, Box<Error>> {
+    pub fn read_value(&self) -> Result<Vec<u8>, Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 
-    pub fn write_value(&self, _values: Vec<u8>) -> Result<(), Box<Error>> {
+    pub fn write_value(&self, _values: Vec<u8>) -> Result<(), Box<dyn Error>> {
         Err(Box::from(NOT_SUPPORTED_ERROR))
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  *  * License, v. 2.0. If a copy of the MPL was not distributed with this
  *   * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
+
 #[cfg(all(target_os = "linux", feature = "bluetooth"))]
 extern crate blurz;
 #[cfg(all(target_os = "android", feature = "bluetooth"))]
@@ -16,3 +16,4 @@ pub mod bluetooth;
               all(target_os = "android", feature = "bluetooth"),
               all(target_os = "macos", feature = "bluetooth"))))]
 mod empty;
+mod macros;
